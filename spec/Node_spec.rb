64a92a689d3bf0fd95906a8ccfb6f64ff704add8
Meg Roth <megroth2@gmail.com>
2023-10-12 19:37:13 -0600

    add Node spec

---
+++ spec/Node_spec.rb
@@ -0,0 +1,23 @@
+require 'pry'
+require './lib/node'
+
+RSpec.describe Node do
+  it 'exists' do
+    node = Node.new("plop")
+
+    binding.pry
+    expect(node).to be_instance_of(Node)
+  end
+
+  it 'has data' do
+    node = Node.new("plop")
+
+    expect(node.data).to eq("plop")
+  end
+
+  it 'has next_node' do
+    node = Node.new("plop")
+
+    expect(node.next_node).to eq(nil)
+  end
+end
