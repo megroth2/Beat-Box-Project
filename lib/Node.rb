a36de7b19381bf3905d647d4b28339b49dedf890
Meg Roth <megroth2@gmail.com>
2023-10-13 15:10:07 -0600

    add comments to Node class and spec

--- lib/Node.rb
+++ lib/Node.rb
@@ -3,6 +3,7 @@ require 'pry'
 class Node
   attr_reader :data, :next_node
 
+  # The method below creates an instance of the node object. It passes the data argument to the @data attribute. It also passes a value of nil to the @next_node attribute.
   def initialize(data)
     @data = data
     @next_node = nil
--- spec/Node_spec.rb
+++ spec/Node_spec.rb
@@ -2,27 +2,32 @@ require 'pry'
 require './lib/node'
 
 RSpec.describe Node do
+  # The test below verifies that calling the initialize method creates an instance of a node object.
   it 'exists' do
     node = Node.new("plop")
 
     expect(node).to be_instance_of(Node)
   end
 
+  # The test below verifies that when a new node is created, an argument is passed in and assigned to the data attribute.
   it 'has data' do
     node = Node.new("plop")
 
     expect(node.data).to eq("plop")
   end
 
-  it 'has another data' do
+  # The test below verifies that the data attribute can be assigned dynamically.
+  it 'has a different data' do
     node = Node.new("doop")
 
     expect(node.data).to eq("doop")
   end
 
+  # The test below verifies that when a new node is created, a nil value is assigned to the next node attribute.
   it 'has next_node' do
     node = Node.new("plop")
 
     expect(node.next_node).to eq(nil)
   end
+
 end
