bb50f2694c2f4c7c1578cafb7ae6c9de95358980
Meg Roth <megroth2@gmail.com>
2023-10-14 22:31:40 -0600

    add append method to linked list class and spec

--- lib/Linked_List.rb
+++ lib/Linked_List.rb
@@ -3,23 +3,41 @@ require './lib/node'
 
 class LinkedList
   attr_reader :head
-  
-  # Create a LinkedList instance with head equal to nil.
+
+   # The method below creates an instance of the linked list object. It passes a value of nil to the @head attribute.
   def initialize
     @head = nil
   end
 
+  # The method below creates a new node and adds it to the end of the list.
+  # Create a new node
+  # if list.head is empty, assign the new node to the list.head
+  # otherwise, iterate through until you find the node in the list where next_node is empty.
+  # Assign the new item as the next_node.
   def append(data)
-    # Create a Node object and set it as the head in the linked list. 
-    # ?? is this written correctly? Should @head be more dynamic? How do I add it to the list without overwriting the head every time?
-    @head = Node.new(data)
+    new_item = Node.new(data)
+    if @head == nil
+      @head = new_item
+    else
+      current_item = @head
+      while current_item.next_node != nil
+        current_item = current_item.next_node 
+      end
+      current_item.next_node = new_item
+    end
   end
 
+  # The method below returns the count of node elements in the linked list.
   def count
-    
+  
   end
 
+  # The method below returns a string of node elements in the linked list.
   def to_string
+    string_length = list.count
+    string_length.times do
+      @node.data # return the data (text) for the given node
+    end
   end
 
 end
--- spec/Linked_List_spec.rb
+++ spec/Linked_List_spec.rb
@@ -3,7 +3,7 @@ require './lib/node'
 require './lib/linked_list'
 
 RSpec.describe LinkedList do
-  # Test that the class can create instances of the "linked_list" object.
+  # The test below verifies that calling the initialize method creates an instance of a linked list object.
   it 'exists' do
     node = Node.new("plop")
     list = LinkedList.new
@@ -11,7 +11,7 @@ RSpec.describe LinkedList do
     expect(list).to be_instance_of(LinkedList)
   end
 
-  # Test that when a linked_list instance is created, the "head" attribute is assigned the nil value
+  # The test below verifies that when a new linked list is created, a nil value is assigned to the head attribute.
   it 'has head' do
     node = Node.new("plop")
     list = LinkedList.new
@@ -19,33 +19,49 @@ RSpec.describe LinkedList do
     expect(list.head).to eq(nil)
   end
 
-  # Test that when the "appends" method is called on a node instance, it adds the node to the list.
-  it 'appends a node to the list' do
+  # The test below verifies that when the appends method is called on a linked list object where head is nil, it creates a new node and sets the head of the linked list.
+  it 'appends a node to the list as the head' do
     list = LinkedList.new
     list.append("doop")
 
-    expect(list.head).to eq(@node)
     expect(list.head.data).to eq("doop")
     expect(list.head.next_node).to eq(nil)
   end
 
-  xit 'appends a second node to list' do
-    node = Node.new("doop")
+  # The test below verifies that when the appends method is called on a linked list object, where head is not nil, it creates a new node and appends it to the linked list.
+  xit 'appends a node to list as the next node' do
     list = LinkedList.new
     list.append("doop")
     list.append("plop")
 
-    expect(list).to eq("doop plop")
+    expect(list.head.next_node.data).to eq("plop")
+    expect(list.head.next_node.next_node).to eq(nil)
   end
 
-  it 'counts how many nodes are in the list' do
-# pry(main)> list.count
-# #=> 1
+   # The test below verifies that when the appends method is called on a linked list object, where head and next node is not nil, it creates a new node and appends it to the linked list.
+   xit 'appends a different node to list as the next node' do
+    list = LinkedList.new
+    list.append("doop")
+    list.append("plop")
+    list.append("pop")
+
+    expect(list.head.next_node.next_node.data).to eq("pop")
+    expect(list.head.next_node..next_node.next_node).to eq(nil)
   end
 
-  it 'generates a string of elements in the list' do
-# pry(main)> list.to_string
-# #=> "doop"
+
+## *****commmit "add appends method to the Linked List class and spec"
+
+
+  xit 'counts how many nodes are in the list' do
+    expect(@list.count).to eq(1)
+  end
+
+  xit 'generates a string of elements in the list' do
+    list = LinkedList.new
+    list.append("doop")
+
+    expect(@list.to_string).to eq("doop")
   end
 
 end
